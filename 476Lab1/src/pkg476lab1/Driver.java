56888f05c22b6d6e1ab957b52ece4203edbd2d46
ColleenRothe <colleenrothe516@gmail.com>
2017-01-26 12:50:15 -0700

    add prints

--- 476Lab1/src/pkg476lab1/Driver.java
+++ 476Lab1/src/pkg476lab1/Driver.java
@@ -17,8 +17,8 @@ import java.util.ArrayList;
 public class Driver {
     private static int dataPieces=0;
     
-    private static String regex = ".*%B\\d{13,19}\\^[a-zA-Z]{2,26}/[a-zA-Z]{2,26}\\^\\d{7}[a-zA-Z0-9]*\\?.*";
-    private static String regex2=".*;\\d{13,19}=\\d{14}[a-zA-Z0-9]*\\?.*";
+    private static String regex = ".*(%B\\d{13,19}\\^[a-zA-Z]{2,26}/[a-zA-Z]{2,26}\\^\\d{7}[a-zA-Z0-9]*\\?).*";
+    private static String regex2=".*(;\\d{13,19}=\\d{14}[a-zA-Z0-9]*\\?).*";
     private static ArrayList <String> track1 = new ArrayList<>();
     private static ArrayList <String> track2 = new ArrayList<>();
 
@@ -38,15 +38,14 @@ public class Driver {
                 Matcher matcher = pattern.matcher(line);
                 boolean isMatched = matcher.matches();
                 if(isMatched == true){
-                    track1.add(line);  
+                    track1.add(matcher.group(1));  
                 }
                 
                 Pattern pattern2 = Pattern.compile(regex2);
                 Matcher matcher2 = pattern2.matcher(line);
                 boolean isMatched2 = matcher2.matches();
                 if(isMatched2 == true){
-                    //get the line...
-                    track2.add(line);  
+                    track2.add(matcher2.group(1));  
 
                 }
                 
@@ -64,16 +63,29 @@ public class Driver {
                 ex.printStackTrace();
             }
         }
+        findData();
     }
     
     public static void findData(){
-          
+        //print to test.....
+        for(int i=0;i<track1.size();i++){
+            System.out.println(track1.get(i));
+        }
+        for(int j=0;j<track2.size();j++){
+            System.out.println(track2.get(j));
+        }
+        //find the PAN for each and check for a match
+        for(int k=0;k<track1.size();k++){
+            String pan = "";
+        }
+        
+        
         printData();        
     }
     
     
     public static void printData(){
-        System.out.println("There is "+dataPieces+" piece of credit card information in the memory data!");
+        System.out.println("There is "+dataPieces+" piece(s) of credit card information in the memory data!");
     }
     
 }
