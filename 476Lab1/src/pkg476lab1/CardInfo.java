bee518ba6c68b0689a9549c6bd2da7df6bb6ba93
ColleenRothe <colleenrothe516@gmail.com>
2017-01-26 23:11:31 -0700

    card

---
+++ 476Lab1/src/pkg476lab1/CardInfo.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg476lab1;
+
+/**
+ *
+ * @author colleenrothe
+ */
+public class CardInfo {
+    private String name;
+    private String cardNum;
+    private String date;
+    private String pin;
+    private String cvvNum;
+    
+    public CardInfo(String name, String cardNum, String date, String pin, String cvvNum){
+        this.name = name;
+        this.cardNum = cardNum;
+        this.date = date;
+        this.pin = pin;
+        this.cvvNum = cvvNum;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    public String getCardNum(){
+        return cardNum;
+    }public String getDate(){
+        return date;
+    }public String getPin(){
+        return pin;
+    }public String getCvvNum(){
+        return cvvNum;
+    }
+    
+       
+}
